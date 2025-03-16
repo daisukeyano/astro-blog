88aa3cca3dee1cbd2072cf3daac2487b79781834
daisuke yano <f1arrows14@gmail.com>
2025-03-16 18:57:02 +0900

    初回コミット

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '神社巡りブログ';
+export const SITE_DESCRIPTION = '訪れた神社を一覧にしていきます。内容はAIも使用しています。使用AI：ChatGPT、felo、Gemini';
